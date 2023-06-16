3279d5771ff16996cde0477a48a0e047f4672974
DrawnInward <jonathan_bean@hotmail.co.uk>
2023-06-16 13:28:54 +0100

    set up devData

--- db/seeds/runSeed.js
+++ db/seeds/runSeed.js
@@ -1,11 +1,12 @@
 const seed = require("./seed");
 const testData = require("../seedData/testData/users.js");
 const connectionPool = require("../connection");
+const devUsers = require("../seedData/devData/devUsers");
 
 const runSeed = () => {
-	return seed(testData).then(() => {
-		connectionPool.close();
-	});
+  return seed(devUsers).then(() => {
+    connectionPool.close();
+  });
 };
 
 runSeed();
